ca1d6c863d0cd7418178b7c8af506d370a303ae4
ronanshakya <ronanshakya98@gmail.com>
2024-04-15 22:23:49 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { InteriorCategoriesComponent } from './interior-categories/interior-categories.component';
+import { InteriorProductComponent } from './interior-product/interior-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'all-products', component: AllProductsComponent },
+  { path: 'product-categories', component: InteriorCategoriesComponent },
+  { path: 'interior-product/:id', component: InteriorProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { InteriorCategoriesComponent } from './interior-categories/interior-categories.component';
+import { InteriorProductComponent } from './interior-product/interior-product.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AllProductsComponent,
+    InteriorCategoriesComponent,
+    InteriorProductComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
